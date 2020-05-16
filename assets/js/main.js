1b73fd7322baee6a4028ddd4ecef80cb01dfe442
stanleynapenas <stan.napenas@gmail.com>
2020-05-17 00:12:36 +0100

    tweak Overflow hidden on sidenav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 function openNav(){
     document.getElementById("mySidenav").style.width = "auto";
+    $(".hero").css({
+        "opacity": "0.5"
+    });
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    $(".hero").css({
+        "opacity": "1"
+    });
 }
 
 $(window).on('load', function () {
@@ -14,7 +20,7 @@ $(window).on('load', function () {
     });
 
     $(".carsText").css({
-        "transition" : "ease-in-out 700ms",
+        "transition" : "ease-in-out 450ms",
         "opacity" : "100%",
     })
 
@@ -25,82 +31,95 @@ $(window).on('load', function () {
     
 })
 
+
 // Transform position horizontaly when a certain CLASS is clicked
 $(".bg-Cars .cliccableCars.modelS").click(function (){
     
     $(".bg-Cars ").css({
-        "transition" : "ease-in-out 700ms",
+        "transition" : "ease-in-out 450ms",
         "transform": "scale(3) translateY(-6%) translateX(30%)"
     })
 
-    $(".hero .carsText .myH1").text("Model S");
+    $(".hero .carsText .myH1").text("Model S").attr("id","left2");
 })
 
 
 $(".bg-Cars .cliccableCars.modelX").click(function (){
 
     $(".bg-Cars ").css({
-        "transition" : "ease-in-out 700ms",
-        "transform": "scale(3) translateY(-6%) translateX(12%)"
+        "transition" : "ease-in-out 450ms",
+        "transform": "scale(3) translateY(-6%) translateX(10%)"
     })
 
-    $(".hero .carsText .myH1").text("Model X").addClass("rightTransition");
+    $(".hero .carsText .myH1").text("Model X").attr("id","left");
     
 })
 
 
 $(".bg-Cars .cliccableCars.model3").click(function (){
     
+    
     $(".bg-Cars ").css({
-        "transition" : "ease-in-out 700ms",
+        "transition" : "ease-in-out 450ms",
         "transform": "scale(3) translateY(-6%) translateX(-12%)"
     })
 
-    $(".hero .carsText .myH1").text("Model 3");
+    $(".hero .carsText .myH1").text("Model 3").attr("id","right2");
 })
 
 $(".bg-Cars .cliccableCars.modelY").click(function (){
     
     $(".bg-Cars ").css({
-        "transition" : "ease-in-out 700ms",
+        "transition" : "ease-in-out 450ms",
         "transform": "scale(3) translateY(-6%) translateX(-30%)"
     })
 
-    $(".hero .carsText .myH1").text("Model Y");
+    $(".hero .carsText .myH1").text("Model Y").attr("id","right");
 })
+
+
 
 // Transform position horizontaly when a certain ID is clicked
 $("#modelS").click(function (){
     
     $(".bg-Cars ").css({
-        "transition" : "ease-in-out 700ms",
+        "transition" : "ease-in-out 450ms",
         "transform": "scale(3) translateY(-6%) translateX(30%)"
     })
+
+    $(".hero .carsText .myH1").text("Model S").attr("id","left2");
 })
 
 
 $("#modelX").click(function (){
-    
+
     $(".bg-Cars ").css({
-        "transition" : "ease-in-out 700ms",
-        "transform": "scale(3) translateY(-6%) translateX(12%)"
+        "transition" : "ease-in-out 450ms",
+        "transform": "scale(3) translateY(-6%) translateX(10%)"
     })
 
+    $(".hero .carsText .myH1").text("Model X").attr("id","left");
+    
 })
 
 
 $("#model3").click(function (){
     
+    
     $(".bg-Cars ").css({
-        "transition" : "ease-in-out 700ms",
+        "transition" : "ease-in-out 450ms",
         "transform": "scale(3) translateY(-6%) translateX(-12%)"
     })
+
+    $(".hero .carsText .myH1").text("Model 3").attr("id","right2");
 })
 
 $("#modelY").click(function (){
     
     $(".bg-Cars ").css({
-        "transition" : "ease-in-out 700ms",
+        "transition" : "ease-in-out 450ms",
         "transform": "scale(3) translateY(-6%) translateX(-30%)"
     })
+
+    $(".hero .carsText .myH1").text("Model Y").attr("id","right");
 })
